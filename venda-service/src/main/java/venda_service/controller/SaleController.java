2e62bc3d291d5c50d61f0c42268ac4c846622fcd
Monteiro712 <jonathanmonteiroa@gmail.com>
2025-06-20 18:55:12 -0300

    clean code

--- venda-service/src/main/java/venda_service/controller/SaleController.java
+++ venda-service/src/main/java/venda_service/controller/SaleController.java
@@ -1,77 +1,46 @@
 package venda_service.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 import venda_service.dto.ProductDTO;
 import venda_service.entities.SaleProduct;
-import venda_service.repository.ProductRepository;
+import venda_service.service.SaleService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class SaleController {
     @Autowired
-    KafkaTemplate<String, String> kafkaTemplate;
-
-    @Autowired
-    private ProductRepository productRepository;
-
-    @Autowired
-    ObjectMapper objectMapper;
+    private SaleService saleService;
 
     @PostMapping("/products")
     public ResponseEntity<SaleProduct> createProduct(@RequestBody ProductDTO productDTO) throws JsonProcessingException {
-        SaleProduct saleProduct = new SaleProduct(productDTO);
-
-        SaleProduct savedProduct = productRepository.save(saleProduct);
-
-        String productJson = objectMapper.writeValueAsString(savedProduct);
-        kafkaTemplate.send("product-topic", productJson);
-        
+        SaleProduct savedProduct = saleService.createProduct(productDTO);
         return ResponseEntity.ok(savedProduct);
     }
 
     @DeleteMapping("/products/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) throws JsonProcessingException {
-        String deleteMessage = objectMapper.writeValueAsString(new ProductDTO(id, null, null, null));
-        kafkaTemplate.send("product-topic", deleteMessage);
-        
-        productRepository.deleteById(id);
+        saleService.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/products/{id}")
     public ResponseEntity<SaleProduct> updateProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) throws JsonProcessingException {
-        return productRepository.findById(id)
-            .map(product -> {
-                product.setName(productDTO.name());
-                product.setPrice(productDTO.price());
-                product.setQuantity(productDTO.quantity());
-                SaleProduct updatedProduct = productRepository.save(product);
-                try {
-                    String productJson = objectMapper.writeValueAsString(updatedProduct);
-                    kafkaTemplate.send("product-topic", productJson);
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
-                }
-                return ResponseEntity.ok(updatedProduct);
-            })
-            .orElse(ResponseEntity.notFound().build());
+        SaleProduct updatedProduct = saleService.updateProduct(id, productDTO);
+        return ResponseEntity.ok(updatedProduct);
     }
 
     @GetMapping("/products")
     public ResponseEntity<List<SaleProduct>> getAllProducts() {
-        return ResponseEntity.ok(productRepository.findAll());
+        return ResponseEntity.ok(saleService.getAllProducts());
     }
 
     @GetMapping("/products/{id}")
     public ResponseEntity<SaleProduct> getProduct(@PathVariable Long id) {
-        return ResponseEntity.ok(productRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("Product not found with ID: " + id)));
-            
+        return ResponseEntity.ok(saleService.getProduct(id));
     }
 }
---
+++ venda-service/src/main/java/venda_service/service/SaleService.java
@@ -0,0 +1,68 @@
+package venda_service.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import venda_service.dto.ProductDTO;
+import venda_service.entities.SaleProduct;
+import venda_service.repository.ProductRepository;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public SaleProduct createProduct(ProductDTO productDTO) throws JsonProcessingException {
+        SaleProduct saleProduct = new SaleProduct(productDTO);
+        SaleProduct savedProduct = productRepository.save(saleProduct);
+        
+        String productJson = objectMapper.writeValueAsString(savedProduct);
+        kafkaTemplate.send("product-topic", productJson);
+        
+        return savedProduct;
+    }
+
+    public void deleteProduct(Long id) throws JsonProcessingException {
+        String delete = objectMapper.writeValueAsString(new ProductDTO(id, null, null, null));
+        kafkaTemplate.send("product-topic", delete);
+        
+        productRepository.deleteById(id);
+    }
+
+    public SaleProduct updateProduct(Long id, ProductDTO productDTO) throws JsonProcessingException {
+        return productRepository.findById(id)
+            .map(product -> {
+                product.setName(productDTO.name());
+                product.setPrice(productDTO.price());
+                product.setQuantity(productDTO.quantity());
+                SaleProduct updatedProduct = productRepository.save(product);
+                try {
+                    String productJson = objectMapper.writeValueAsString(updatedProduct);
+                    kafkaTemplate.send("product-topic", productJson);
+                    return updatedProduct;
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            })
+            .orElseThrow(() -> new RuntimeException("Product not found with ID: " + id));
+    }
+
+    public List<SaleProduct> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public SaleProduct getProduct(Long id) {
+        return productRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Product not found with ID: " + id));
+    }
+}
